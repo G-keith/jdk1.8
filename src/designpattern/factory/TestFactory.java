08c5ac05a0baa5db521fa939801a01eed728aa66
G-keith <1605763261@qq.com>
2019-05-15 16:17:16 +0800

    nio案例

--- src/designpattern/factory/TestFactory.java
+++ src/designpattern/factory/TestFactory.java
@@ -9,6 +9,8 @@ public class TestFactory {
 
     public static void main(String[] args) {
         People man=PeopleFactory.getPeople("man","男人","男",25);
+        People woman=PeopleFactory.getPeople("woman","女人","女",25);
         System.out.println(man);
+        System.out.println(woman);
     }
 }
