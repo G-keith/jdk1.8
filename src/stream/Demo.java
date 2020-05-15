764aa61680c9ff77a96c2a3b7cb39bf626435c47
keith <G-keith>
2020-05-15 09:11:59 +0800

    stream  分组计数，求和等

--- src/stream/Demo.java
+++ src/stream/Demo.java
@@ -1,9 +1,7 @@
 package stream;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Vector;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -21,12 +19,15 @@ public class Demo {
         userList.add(new User("小红",20,"女"));
         userList.add(new User("小明",25,"男"));
         userList.add(new User("张三",50,"男"));
+        userList.add(new User("张三",29,"男"));
 //        userList.forEach(System.out::println);
 //        //流的删选写法
 //        List<User> filterList=userList.stream().filter(user -> user.getAge()>20).collect(toList());
 //        System.out.println("删选写法："+filterList);
 //        //去重
 //        //List<User> distinctList=userList.stream().distinct().collect(toList());
+//        System.out.println("去重写法："+filterList);
+
 //        //截取
 //        List<User> limitList=userList.stream().limit(2).collect(toList());
 //        System.out.println("截取写法："+limitList);
@@ -39,6 +40,18 @@ public class Demo {
         //分组写法
         Map<String,List<User>> listMap=userList.stream().collect(Collectors.groupingBy(User::getSex));
         System.out.println(listMap);
+        //分组计数
+        Map<String,Long> collect=userList.stream().collect(Collectors.groupingBy(User::getSex,Collectors.counting()));
+        System.out.println("分组计数："+collect);
+        //"分组求和"
+        Map<String,Long> collect2=userList.stream().collect(Collectors.groupingBy(User::getSex,Collectors.summingLong(User::getAge)));
+        System.out.println("分组求和："+collect2);
+        Map<String,Long> collect3=userList.stream().collect(Collectors.groupingBy((e-> e.getSex()+"_"+e.getName()),Collectors.summingLong(User::getAge)));
+        System.out.println("多字段分组求和："+collect3);
+        TreeMap<String,Long> collect4=userList.stream().collect(Collectors.groupingBy(User::getSex, TreeMap::new,Collectors.summingLong(User::getAge)));
+        System.out.println("分组升序排序："+collect4);
+        NavigableMap<String,Long> collect5=userList.stream().collect(Collectors.groupingBy(User::getSex, TreeMap::new ,Collectors.summingLong(User::getAge))).descendingMap();
+        System.out.println("分组降序排序："+collect5);
         //设置值
 //        userList.forEach(item->{
 //            item.getName();
